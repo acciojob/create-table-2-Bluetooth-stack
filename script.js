7fee171e7e686a1b92f563b1b1ed362bd7022401
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-21 01:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
     //Write your code here
-  
+	let row = prompt('input number of rows');
+	while(row==''){
+		row = prompt('please enter number of rows');
+	}
+	let col = prompt('input number of columns');
+	row = parseInt(row);
+	col = parseInt(col);
+	// console.log(row, col)
+	const tab = document.getElementById('myTable');
+	for(let i = 0; i<row; i++){
+		let tr = document.createElement('tr');
+		for(let j = 0; j<col; j++){
+			let td = document.createElement('td');
+			td.innerText = `Row-${i} Column-${j}`
+			tr.appendChild(td);
+		}
+		tab.appendChild(tr);
+	}
 }
